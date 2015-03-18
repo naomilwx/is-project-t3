7cde405f464f59a82e451446166a3cfe14273088
Naomi Leow <naomileowwx@gmail.com>
2015-03-18 02:38:00 +0100

    with object implementations

--- service-impl/src/main/java/ch/ethz/globis/isk/config/PersistenceConfig.java
+++ service-impl/src/main/java/ch/ethz/globis/isk/config/PersistenceConfig.java
@@ -1,6 +1,6 @@
 package ch.ethz.globis.isk.config;
 
-//import ch.ethz.globis.isk.domain.mongo.*;
+import ch.ethz.globis.isk.domain.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
